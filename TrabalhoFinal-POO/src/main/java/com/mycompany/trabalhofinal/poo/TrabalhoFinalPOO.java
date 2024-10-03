c828bdae29b2dafbe886c2de942f2765952b5c79
GabrielVitor08 <gabrieldatas2004@gmail.com>
2024-10-03 19:40:37 -0300

    feat: Initial commit

---
+++ TrabalhoFinal-POO/src/main/java/com/mycompany/trabalhofinal/poo/TrabalhoFinalPOO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabalhofinal.poo;
+
+/**
+ *
+ * @author gabriel
+ */
+public class TrabalhoFinalPOO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
